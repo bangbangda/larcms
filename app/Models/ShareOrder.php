8b01560d2953a6bd59a9846189f9757442402b06
HaoLiang <haoliang@outlook.jp>
2020-12-07 23:07:46 +0800

    Fix bug 优化发放分享红包逻辑

--- app/Models/ShareOrder.php
+++ app/Models/ShareOrder.php
@@ -11,7 +11,7 @@ class ShareOrder extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $fillable = ['customer_id', 'sub_customer_id'];
+    protected $fillable = ['customer_id', 'sub_customer_id', 'sub_openid'];
 
     protected $hidden = ['pay_state', 'sub_customer_id', 'deleted_at', 'updated_at'];
 
