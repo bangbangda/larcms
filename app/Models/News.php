e57bc17105569c26aed4cfead1b789576b4eda2e
HaoLiang <haoliang@outlook.jp>
2020-12-12 21:49:39 +0800

    功能更新 增加软删除

--- app/Models/News.php
+++ app/Models/News.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class News extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $hidden = [
         'id', 'deleted_at', 'created_at', 'updated_at'
