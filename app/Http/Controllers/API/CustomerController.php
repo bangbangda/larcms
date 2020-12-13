28f2d7a1407277ca32e5eca104ce5398677bf6c0
HaoLiang <haoliang@outlook.jp>
2020-12-13 15:17:04 +0800

    功能更新 处理2条数据的问题

--- app/Http/Controllers/API/CustomerController.php
+++ app/Http/Controllers/API/CustomerController.php
@@ -111,10 +111,25 @@ class CustomerController extends Controller
             $decryptedData = $miniApp->encryptor->decryptData(
                 $customer->session_key, $request->post('iv'), $request->post('encryptedData')
             );
+
             // 更新手机号码
             $customer->update([
-                'phone' => $decryptedData['phoneNumber']
+                'phone' => $decryptedData['phoneNumber'],
             ]);
+
+            // 处理多重数据的问题
+            $miniCustomer = Customer::where('openid', $customer['openid'])
+                ->whereNull('mp_openid')
+                ->whereNotNull('parent_id')
+                ->first();
+            // 更新上级
+            if ($miniCustomer) {
+                $customer->update([
+                    'parent_id' => $miniCustomer['parent_id'],
+                ]);
+
+                $customer = Customer::find($customer['id']);
+            }
 
             // 触发绑定手机号事件
             event(new CustomerPhoneBound($customer));
