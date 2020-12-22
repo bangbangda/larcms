becbe1eb5a2525018a1cdc01fa5a301796a6acb9
HaoLiang <haoliang@outlook.jp>
2020-12-22 18:04:34 +0800

    功能更新 临时去掉用户注册

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -26,7 +26,7 @@ class CreateNewUser implements CreatesNewUsers
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
         ])->validate();
-
+        return ;
         return DB::transaction(function () use ($input) {
             return tap(User::create([
                 'name' => $input['name'],
