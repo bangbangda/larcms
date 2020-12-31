902c71df572437bf2243a3ecd6ba0ff15e7e8b89
HaoLiang <haoliang@outlook.jp>
2020-12-31 23:28:39 +0800

    功能更新 薇格短信平台接口

---
+++ app/Services/VgSms.php
@@ -0,0 +1,97 @@
+<?php
+namespace App\Services;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+/**
+ * 微格短信接口
+ *
+ * Class VgSms
+ * @package App\Services
+ */
+class VgSms
+{
+    // 接口网关
+    private string $apiUrl = 'http://47.110.195.237:8081';
+
+    /**
+     * 发送短信
+     *   支持批量进件，单次最多支持1000个手机号
+     *
+     * @param  string  $content 短信内容
+     * @param  array  $mobile 接受手机号码
+     * @return array 结果
+     */
+    public function send(string $content, array $mobile) : array
+    {
+        $result = Http::asForm()->post($this->apiUrl . '/api/sms/send', [
+            'userid' => config('vgsms.user_id'),
+            'ts' => $ts = Carbon::now()->getPreciseTimestamp(3),
+            'sign' => $this->generateSign($ts),
+            'mobile' => implode(',', $mobile),
+            'msgcontent' => $content,
+        ])->json();
+
+        if ($result['code'] > 0) {
+            Log::error("短信发送失败：{$result['msg']}");
+        }
+
+        return $result;
+    }
+
+
+    /**
+     * 短信剩余条数查询
+     *
+     * @return array
+     */
+    public function balance() : array
+    {
+        $result = Http::asForm()->post($this->apiUrl . '/api/sms/balance', [
+            'userid' => config('vgsms.user_id'),
+            'ts' => $ts = Carbon::now()->getPreciseTimestamp(3),
+            'sign' => $this->generateSign($ts),
+        ])->json();
+
+        if ($result['code'] > 0) {
+            Log::error("短信剩余条数查询失败：{$result['msg']}");
+        }
+
+        return $result;
+    }
+
+    /**
+     * 短信内容敏感词验证
+     *
+     * @param  string  $content 短信内容
+     * @return array
+     */
+    public function contentSecCheck(string $content) : array
+    {
+        $result = Http::asForm()->post($this->apiUrl . '/api/sms/balance', [
+            'userid' => config('vgsms.user_id'),
+            'ts' => $ts = Carbon::now()->getPreciseTimestamp(3),
+            'sign' => $this->generateSign($ts),
+            'msgcontent' => $content,
+        ])->json();
+
+        if ($result['code'] > 0) {
+            Log::error("短信敏感词检验失败：{$result['msg']}");
+        }
+
+        return $result;
+    }
+
+    /**
+     * 加密签名
+     *
+     * @param  string  $ts
+     * @return string
+     */
+    private function generateSign(string $ts) : string
+    {
+        return md5(config('vgsms.user_id') . $ts . config('vgsms.api_key'));
+    }
+}
