b532e24b04215f66f9ee40811734d57c190e43e5
haoliang <haoliang@outlook.jp>
2021-07-30 17:48:16 +0800

    Fix 修复无法更新公众号二维码的bug

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -24,7 +24,7 @@ class Customer extends Model
 
     protected $fillable = [
         'unionid', 'openid', 'mp_openid', 'session_key', 'phone', 'nickname', 'avatar_url',
-        'qrcode_url', 'subscribe_scene', 'qr_scene', 'qr_scene_str', 'parent_id'
+        'qrcode_url', 'mp_qrcode_url', 'subscribe_scene', 'qr_scene', 'qr_scene_str', 'parent_id'
     ];
 
     protected $casts = [
