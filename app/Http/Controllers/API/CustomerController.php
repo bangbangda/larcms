a25687c90c9fd423b53d481702e28799177b284c
haoliang <haoliang@outlook.jp>
2021-08-08 14:59:59 +0800

    更新活动范围距离

--- app/Http/Controllers/API/CustomerController.php
+++ app/Http/Controllers/API/CustomerController.php
@@ -68,7 +68,7 @@ class CustomerController extends Controller
             'token' => $customer->updateToken('mini-app')->plainTextToken,
             'bindPhone' => $customer->hasBindPhone(),
             'bindMp' => $customer->hasSubscribeMp(),
-            'distance' => 1500 * 1000, // 可领取红包距离 单位米
+            'distance' => 32 * 1000, // 可领取红包距离 单位米
         ]);
     }
 
