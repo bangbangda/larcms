c3a12c3b94da15db9f821dfe2e6eb2cacc67096e
haoliang <haoliang@outlook.jp>
2021-08-09 13:26:36 +0800

    优化 发送现金红包命令

--- app/Console/Commands/WechatSendRedPack.php
+++ app/Console/Commands/WechatSendRedPack.php
@@ -44,7 +44,7 @@ class WechatSendRedPack extends Command
     {
         // 查询发送失败红包
         $reSendData = TransferLog::whereNull('payment_no')->where('created_at', '<', now()->toDateTimeString())
-            ->where('customer_id', '117')
+            ->where('customer_id', '9400')
             ->select('customer_id', DB::raw('sum(amount) as s_amount'))
             ->groupBy('customer_id')
             ->having('s_amount', '>', 100)
@@ -52,7 +52,6 @@ class WechatSendRedPack extends Command
 
         foreach ($reSendData as $data) {
             $customer = Customer::find($data->customer_id);
-
             SendWechatRedPack::dispatch([
                 'mp_openid' => $customer->mp_openid,
                 'amount' => $data['s_amount'],
